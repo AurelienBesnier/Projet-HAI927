6872fc8556604965129477a95321a235ca05a7d1
Aurelien Besnier <aurelienbesnier.pro@gmail.com>
2022-11-28 10:40:48 +0100

    Adding start of obscuring script

---
+++ src/ObscureDataSet/obscuredataset.cpp
@@ -0,0 +1,41 @@
+#include <string_view>
+#include <filesystem>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <algorithm>
+
+#include <opencv2/imgproc.hpp>
+
+
+void pixel(cv::Mat& img, cv::Rect roi, int size)
+{
+    roi.height = (roi.height + size) / (size + 1) * (size + 1);
+    roi.width = (roi.width + size) / (size + 1) * (size + 1);
+    if(roi.height + roi.y > img.rows || roi.width + roi.x > img.cols)
+    {
+        return;
+    }
+    cv::Mat area = img(roi);
+    cv::Mat smol(roi.height / (size + 1), roi.width / (size + 1), area.type());
+    cv::resize(area, smol, smol.size(), 0, 0, cv::INTER_AREA);
+    cv::resize(smol, area, area.size(), 0, 0, cv::INTER_NEAREST);
+}
+
+void blur(cv::Mat& img, cv::Rect roi, int radius)
+{
+    cv::GaussianBlur(img(roi), img(roi), {radius*2+1,radius*2+1}, 0);
+}
+
+void blackhead(cv::Mat& img, cv::Rect roi)
+{
+    img(roi) *= 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+
+  return EXIT_SUCCESS;
+}
