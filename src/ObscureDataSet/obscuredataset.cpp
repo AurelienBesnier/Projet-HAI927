742a0e11314732452ebe4e13e824536264331b25
Aurelien Besnier <aurelienbesnier.pro@gmail.com>
2022-11-28 12:26:52 +0100

    Adding skeleton to obscuration dataset

--- src/ObscureDataSet/obscuredataset.cpp
+++ src/ObscureDataSet/obscuredataset.cpp
@@ -8,6 +8,10 @@
 
 #include <opencv2/imgproc.hpp>
 
+void blur(cv::Mat& img, cv::Rect roi, int radius)
+{
+    cv::GaussianBlur(img(roi), img(roi), {radius*2+1,radius*2+1}, 0);
+}
 
 void pixel(cv::Mat& img, cv::Rect roi, int size)
 {
@@ -23,19 +27,25 @@ void pixel(cv::Mat& img, cv::Rect roi, int size)
     cv::resize(smol, area, area.size(), 0, 0, cv::INTER_NEAREST);
 }
 
-void blur(cv::Mat& img, cv::Rect roi, int radius)
-{
-    cv::GaussianBlur(img(roi), img(roi), {radius*2+1,radius*2+1}, 0);
-}
-
 void blackhead(cv::Mat& img, cv::Rect roi)
 {
     img(roi) *= 0;
 }
 
+cv::Rect getRoiFromFaceloc(std::filesystem::path file, std::filesystem::path faceloc_file, int obscuration_method)
+{
+    // TODO: Obscure the file with the corresponding faceloc and method
+    // if method = 1 - > blur
+    // if method = 2 - > pixel
+    // if method = 3 - > blackhead
+  
+}
 
 int main(int argc, char* argv[])
 {
+
+  if(argc!=3) return EXIT_FAILURE;
+
 
   return EXIT_SUCCESS;
 }
